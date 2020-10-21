62f5504f060892e6efaf0db422a0dca81c2e6c0f
slqpez <santiago.lopez16@udea.edu.co>
2020-10-20 20:46:06 -0500

    register activity, and intent

---
+++ Labs/LabsCM20201/lab1activities/src/main/java/co/edu/udea/compumovil/gr03_20201/lab1activities/RegisterActivity.java
@@ -0,0 +1,13 @@
+package co.edu.udea.compumovil.gr03_20201.lab1activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
