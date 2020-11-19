0ffe431311e5390fef67dfff4ef9b9dc5b999650
unknown <j98fer@gmail.com>
2020-11-18 21:06:01 -0500

    PullApp

---
+++ pullapp/lib/pages/calculadora_imc_pages.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Calculadora extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        title: new Text("Quienes somos"),
+      ),
+      body: new Center(
+        child: new Text('implementar calculadora aqui'),
+      ),
+    );
+  }
+}
---
+++ pullapp/lib/pages/nosotros_pages.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Nosotros extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        title: new Text("Quienes somos"),
+      ),
+      body: new Center(
+        child: new Text('Aqui vamos a dar soporte'),
+      ),
+    );
+  }
+}
